55f2ab429d6fb32a2ea1c5c7e9d3ce08e8d98a09
Alex Kovar <ajkovar@gmail.com>
2020-09-04 23:33:17 -0500

    added code for replacing jsx elements

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,45 @@
-import acorn = require("acorn");
 import fs = require("fs");
-import jsx = require("acorn-jsx");
+import * as parser from "@babel/parser";
+import traverse = require("@babel/traverse");
+import generate from "@babel/generator";
+import * as t from "@babel/types";
+import prettier from "prettier";
+
 const file = fs.readFileSync("./sample/Cardlayout.jsx");
-var JSXParser = acorn.Parser.extend(jsx());
-const ast: acorn.Node = JSXParser.parse(file.toString(), {
-	ecmaVersion: 2020,
-	sourceType: "module",
+const ast = parser.parse(file.toString(), {
+  sourceType: "module",
+  plugins: ["jsx"],
 });
-console.log(ast);
+
+traverse.default(ast, {
+  JSXText: function (path) {
+    if (path.node.value.trim() !== "") {
+      path.replaceWith(
+        t.jsxElement(
+          t.jsxOpeningElement(
+            t.jsxIdentifier("FormattedMessage"),
+            [
+              t.jsxAttribute(
+                t.jsxIdentifier("description"),
+                t.stringLiteral(path.node.value.trim())
+              ),
+            ],
+            false
+          ),
+          t.jsxClosingElement(t.jsxIdentifier("FormattedMessage")),
+          []
+        )
+      );
+    }
+  },
+});
+
+fs.writeFileSync(
+  "sample/output.jsx",
+  prettier.format(generate(ast).code, {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+  })
+);
