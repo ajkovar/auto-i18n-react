d1afac0a2881f8f84dc01df28402ba1bbc3fc55e
Alex Kovar <ajkovar@gmail.com>
2020-09-06 08:25:50 -0500

    add HOC wrapper and prop variable setting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import fs = require("fs");
 import * as parser from "@babel/parser";
-import traverse = require("@babel/traverse");
+import traverse, { NodePath } from "@babel/traverse";
 import generate from "@babel/generator";
 import * as t from "@babel/types";
 import prettier from "prettier";
-import { NodePath } from "@babel/traverse";
 import findContainingReactClass from "./util/findContainingReactClass";
+import findContainer from "./util/findContainer";
+import template from "@babel/template";
 
 const whitelistedAttributes = "subtitle text noText yesText label buttonCTAText title ctaLinkText".split(
   " "
@@ -17,7 +18,9 @@ const ast = parser.parse(file.toString(), {
 });
 
 let hasFormattedMessageImport = false;
-traverse.default(ast, {
+let injectIntlImportNeeded = false;
+let parentClass: NodePath<t.ClassDeclaration> | null;
+traverse(ast, {
   ImportDeclaration: function (path) {
     hasFormattedMessageImport =
       hasFormattedMessageImport ||
@@ -49,7 +52,7 @@ traverse.default(ast, {
     }
   },
   StringLiteral: function (path) {
-    const parentClass = findContainingReactClass(path as NodePath<t.Node>);
+    parentClass = findContainingReactClass(path as NodePath<t.Node>);
     const allowedContainers = [
       "JSXExpressionContainer",
       "ConditionalExpression",
@@ -61,9 +64,38 @@ traverse.default(ast, {
     const textRegexp = /[A-Z\.\,\!\:]/;
     if (
       allowedContainers.includes(path.parentPath.node.type) &&
-      textRegexp.exec(path.node.value) && 
+      textRegexp.exec(path.node.value) &&
       parentClass
     ) {
+      const classMethod = findContainer(
+        path as NodePath<t.Node>,
+        "ClassMethod"
+      ) as NodePath<t.ClassMethod>;
+      //   const topLevelBlock = (classMethod?.node as t.ClassMethod).body;
+      //   topLevelBlock.body.some(
+      //     (statement) =>
+      //       statement.type === "VariableDeclaration" &&
+      //       statement.declarations.some(
+      //         (declarator) =>
+      //           declarator.id.type === "ObjectPattern" &&
+      //           declarator.id.properties.some(
+      //             (property) => property.key.name == "intl"
+      //           )
+      //       )
+      //   );
+      if (!path.scope.hasBinding("intl")) {
+        // classMethod
+        //   ?.get("body")
+        //   .unshiftContainer("body", template.ast("const {intl} = this.props;"));
+        classMethod?.get("body").scope.push({
+          id: t.identifier("intl"),
+          init: t.memberExpression(
+            t.memberExpression(t.thisExpression(), t.identifier("props")),
+            t.identifier("intl")
+          ),
+          //   template.ast("const {intl} = this.props;")
+        });
+      }
       const intlCallExpression = t.callExpression(
         t.memberExpression(t.identifier("intl"), t.identifier("formatMessage")),
         [t.stringLiteral(path.node.value)]
@@ -76,67 +108,103 @@ traverse.default(ast, {
       path.skip();
     }
   },
-//   JSXAttribute: function (path) {
-//     // TODO move this to StringLiteral selector
-//     const whitelistedAttributes = "subtitle text noText yesText label buttonCTAText title ctaLinkText".split(
-//       " "
-//     );
-//     const node = path.node as t.JSXAttribute;
-//     const attrName = node.name.name.toString();
-//     if (whitelistedAttributes.includes(attrName)) {
-//       const parentClass = findContainingReactClass(path as NodePath<t.Node>);
-//       //   console.log(attrName);
-//       //   const conditional =
-//       //     node.value?.type === "JSXExpressionContainer" &&
-//       //     (node.value as t.JSXExpressionContainer).expression.type ===
-//       //       "ConditionalExpression" &&
-//       //     (((node.value as t.JSXExpressionContainer)
-//       //       .expression as t.ConditionalExpression).alternate.type ===
-//       //       "StringLiteral" ||
-//       //       ((node.value as t.JSXExpressionContainer)
-//       //         .expression as t.ConditionalExpression).consequent.type ===
-//       //         "StringLiteral");
-//       //   const literal =
-//       //     node.value?.type === "JSXExpressionContainer" &&
-//       //     (node.value as t.JSXExpressionContainer).expression.type ===
-//       //       "StringLiteral";
-//       if (parentClass) {
-//         path.replaceWith(
-//           t.jsxAttribute(
-//             t.jsxIdentifier(attrName),
-//             t.jsxExpressionContainer(
-//               t.callExpression(
-//                 // t.memberExpression(
-//                 //   t.identifier("this"),
-//                 t.memberExpression(
-//                   t.identifier("intl"),
-//                   t.identifier("formatMessage")
-//                 ),
-//                 // ),
-//                 // [t.stringLiteral(node.value?.value)]
-//                 [t.stringLiteral("test")]
-//               )
-//             )
-//           )
-//         );
-//       }
-//       path.skip();
-//     }
-//   },
+  ExportDefaultDeclaration(path) {
+    traverse(
+      path.node,
+      {
+        Identifier(path) {
+          if (!parentClass) {
+            return;
+          }
+          const className = parentClass.node.id.name;
+          if (path.node.name === className) {
+            injectIntlImportNeeded = !path.scope.hasBinding("injectIntl");
+            path.replaceWith(
+              t.callExpression(t.identifier("injectIntl"), [
+                t.identifier(className),
+              ])
+            );
+            path.skip();
+          }
+        },
+      },
+      path.scope,
+      null,
+      path.parentPath
+    );
+    path.skip();
+  },
+  //   JSXAttribute: function (path) {
+  //     // TODO move this to StringLiteral selector
+  //     const whitelistedAttributes = "subtitle text noText yesText label buttonCTAText title ctaLinkText".split(
+  //       " "
+  //     );
+  //     const node = path.node as t.JSXAttribute;
+  //     const attrName = node.name.name.toString();
+  //     if (whitelistedAttributes.includes(attrName)) {
+  //       const parentClass = findContainingReactClass(path as NodePath<t.Node>);
+  //       //   console.log(attrName);
+  //       //   const conditional =
+  //       //     node.value?.type === "JSXExpressionContainer" &&
+  //       //     (node.value as t.JSXExpressionContainer).expression.type ===
+  //       //       "ConditionalExpression" &&
+  //       //     (((node.value as t.JSXExpressionContainer)
+  //       //       .expression as t.ConditionalExpression).alternate.type ===
+  //       //       "StringLiteral" ||
+  //       //       ((node.value as t.JSXExpressionContainer)
+  //       //         .expression as t.ConditionalExpression).consequent.type ===
+  //       //         "StringLiteral");
+  //       //   const literal =
+  //       //     node.value?.type === "JSXExpressionContainer" &&
+  //       //     (node.value as t.JSXExpressionContainer).expression.type ===
+  //       //       "StringLiteral";
+  //       if (parentClass) {
+  //         path.replaceWith(
+  //           t.jsxAttribute(
+  //             t.jsxIdentifier(attrName),
+  //             t.jsxExpressionContainer(
+  //               t.callExpression(
+  //                 // t.memberExpression(
+  //                 //   t.identifier("this"),
+  //                 t.memberExpression(
+  //                   t.identifier("intl"),
+  //                   t.identifier("formatMessage")
+  //                 ),
+  //                 // ),
+  //                 // [t.stringLiteral(node.value?.value)]
+  //                 [t.stringLiteral("test")]
+  //               )
+  //             )
+  //           )
+  //         );
+  //       }
+  //       path.skip();
+  //     }
+  //   },
 });
 
-let code = hasFormattedMessageImport
-  ? ""
-  : "import {FormattedMessage} from 'react-intl';";
-code += generate(ast).code;
+const imports = {
+  FormattedMessage: !hasFormattedMessageImport,
+  injectIntl: injectIntlImportNeeded,
+};
+
+const importsString = Object.entries(imports)
+  .filter(([key, value]) => value)
+  .map(([key]) => key)
+  .join(", ");
+
+const code =
+  (importsString.length === 0
+    ? ""
+    : `import {${importsString}} from 'react-intl';`) + generate(ast).code;
 
 fs.writeFileSync(
   "sample/output.jsx",
-  code
-  // prettier.format(code, {
-  //   trailingComma: "es5",
-  //   tabWidth: 2,
-  //   semi: true,
-  //   singleQuote: true,
-  // })
+//   code
+  prettier.format(code, {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+  })
 );
--- src/util/findContainingReactClass.ts
+++ src/util/findContainingReactClass.ts
@@ -1,13 +1,13 @@
 import { NodePath } from "@babel/traverse";
 import * as t from "@babel/types";
 
-function findContainingReactClass(path: NodePath): NodePath | null {
+function findContainingReactClass(path: NodePath): NodePath<t.ClassDeclaration> | null {
   if (
     path.node.type === "ClassDeclaration" &&
     ((path.node.superClass as t.MemberExpression)?.object as any).name ===
       "React"
   ) {
-    return path;
+    return path as NodePath<t.ClassDeclaration>;
   }
   if (path.parentPath) {
     return findContainingReactClass(path.parentPath);
