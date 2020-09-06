9fd6f3a14c2890286df7c2928935ccf7236c1011
Alex Kovar <ajkovar@gmail.com>
2020-09-05 22:49:26 -0500

    improved version using string literal replacement

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import prettier from "prettier";
 import { NodePath } from "@babel/traverse";
 import findContainingReactClass from "./util/findContainingReactClass";
 
+const whitelistedAttributes = "subtitle text noText yesText label buttonCTAText title ctaLinkText".split(
+  " "
+);
 const file = fs.readFileSync("./sample/CardLayout.jsx");
 const ast = parser.parse(file.toString(), {
   sourceType: "module",
@@ -45,54 +48,82 @@ traverse.default(ast, {
       path.skip();
     }
   },
-  JSXAttribute: function (path) {
-    // TODO move this to StringLiteral selector
-    const whitelistedAttributes = "subtitle text noText yesText label buttonCTAText title ctaLinkText".split(
-      " "
-    );
-    const node = path.node as t.JSXAttribute;
-    const attrName = node.name.name.toString();
-    if (whitelistedAttributes.includes(attrName)) {
-      const parentClass = findContainingReactClass(path as NodePath<t.Node>);
-      console.log(attrName);
-    //   const conditional =
-    //     node.value.type === "JSXExpressionContainer" &&
-    //     (node.value as t.JSXExpressionContainer).expression.type ===
-    //       "ConditionalExpression" &&
-    //     (((node.value as t.JSXExpressionContainer)
-    //       .expression as t.ConditionalExpression).alternate.type ===
-    //       "StringLiteral" ||
-    //       ((node.value as t.JSXExpressionContainer)
-    //         .expression as t.ConditionalExpression).consequent.type ===
-    //         "StringLiteral");
-    //   const literal =
-    //     node.value.type === "JSXExpressionContainer" &&
-    //     (node.value as t.JSXExpressionContainer).expression.type === "StringLiteral" 
-      if (parentClass) {
-        path.replaceWith(
-          t.jsxAttribute(
-            t.jsxIdentifier(attrName),
-            t.jsxExpressionContainer(
-              t.callExpression(
-                // t.memberExpression(
-                //   t.identifier("this"),
-                t.memberExpression(
-                  t.identifier("intl"),
-                  t.identifier("formatMessage")
-                ),
-                // ),
-                // [t.stringLiteral(node.value?.value)]
-                [t.stringLiteral("test")]
-              )
-            )
-          )
-        );
-      }
+  StringLiteral: function (path) {
+    const parentClass = findContainingReactClass(path as NodePath<t.Node>);
+    const allowedContainers = [
+      "JSXExpressionContainer",
+      "ConditionalExpression",
+      "JSXAttribute",
+      "ObjectProperty",
+    ];
+    // Assume things with capital letters or certain punctuation are translatable.
+    // This may need to be adjusted
+    const textRegexp = /[A-Z\.\,\!\:]/;
+    if (
+      allowedContainers.includes(path.parentPath.node.type) &&
+      textRegexp.exec(path.node.value) && 
+      parentClass
+    ) {
+      const intlCallExpression = t.callExpression(
+        t.memberExpression(t.identifier("intl"), t.identifier("formatMessage")),
+        [t.stringLiteral(path.node.value)]
+      );
+      path.replaceWith(
+        path.parentPath.node.type === "JSXAttribute"
+          ? t.jsxExpressionContainer(intlCallExpression)
+          : intlCallExpression
+      );
       path.skip();
     }
   },
+//   JSXAttribute: function (path) {
+//     // TODO move this to StringLiteral selector
+//     const whitelistedAttributes = "subtitle text noText yesText label buttonCTAText title ctaLinkText".split(
+//       " "
+//     );
+//     const node = path.node as t.JSXAttribute;
+//     const attrName = node.name.name.toString();
+//     if (whitelistedAttributes.includes(attrName)) {
+//       const parentClass = findContainingReactClass(path as NodePath<t.Node>);
+//       //   console.log(attrName);
+//       //   const conditional =
+//       //     node.value?.type === "JSXExpressionContainer" &&
+//       //     (node.value as t.JSXExpressionContainer).expression.type ===
+//       //       "ConditionalExpression" &&
+//       //     (((node.value as t.JSXExpressionContainer)
+//       //       .expression as t.ConditionalExpression).alternate.type ===
+//       //       "StringLiteral" ||
+//       //       ((node.value as t.JSXExpressionContainer)
+//       //         .expression as t.ConditionalExpression).consequent.type ===
+//       //         "StringLiteral");
+//       //   const literal =
+//       //     node.value?.type === "JSXExpressionContainer" &&
+//       //     (node.value as t.JSXExpressionContainer).expression.type ===
+//       //       "StringLiteral";
+//       if (parentClass) {
+//         path.replaceWith(
+//           t.jsxAttribute(
+//             t.jsxIdentifier(attrName),
+//             t.jsxExpressionContainer(
+//               t.callExpression(
+//                 // t.memberExpression(
+//                 //   t.identifier("this"),
+//                 t.memberExpression(
+//                   t.identifier("intl"),
+//                   t.identifier("formatMessage")
+//                 ),
+//                 // ),
+//                 // [t.stringLiteral(node.value?.value)]
+//                 [t.stringLiteral("test")]
+//               )
+//             )
+//           )
+//         );
+//       }
+//       path.skip();
+//     }
+//   },
 });
-console.log(hasFormattedMessageImport);
 
 let code = hasFormattedMessageImport
   ? ""
